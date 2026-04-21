933427405c8e04d17de5a0e83ce8bbe26fa598b6
Olena Kulynyak <kulynyakolena@gmail.com>
2026-04-21 22:42:45 +0300

    Add delete node in bst  solution

---
+++ delete-node-in-bst/code_delete.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+            next_val = root.right
+            while next_val.left:
+                next_val = next_val.left
+
+            root.val = next_val.val
+            root.right = self.deleteNode(root.right, next_val.val)
+
+        return root
